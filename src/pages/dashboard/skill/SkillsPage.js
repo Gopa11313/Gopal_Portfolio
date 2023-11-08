41fadb5459d5ff98d032f692cd6c9179097a5497
Gopa11313 <gopalaaa9819525993@gmail.com>
2023-11-07 22:31:36 -0500

    skill page

--- src/pages/dashboard/skill/SkillsPage.js
+++ src/pages/dashboard/skill/SkillsPage.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import SkillCss from "../../../styles/Skillscss.css";
 import { useTabs, TabPanel } from "react-headless-tabs";
+import KotlinImage from "../../../assests/kotlin_image.png";
+import JavaImage from "../../../assests/java_image.png";
+import PythonImage from "../../../assests/python_image.png";
+import SwiftImage from "../../../assests/swift_image.png";
 export default function SkillsPage() {
   const [value, setValue] = React.useState("1");
 
@@ -10,6 +14,18 @@ export default function SkillsPage() {
     "IDE",
     "Database",
   ]);
+  const languages = [
+    { name: "Kotlin", image: KotlinImage },
+    { name: "Java", image: JavaImage },
+    { name: "Python", image: PythonImage },
+    { name: "Swift", image: SwiftImage },
+    { name: "Java", image: JavaImage },
+    { name: "Python", image: PythonImage },
+    { name: "Python", image: PythonImage },
+    { name: "Python", image: PythonImage },
+    { name: "Python", image: PythonImage },
+    { name: "Python", image: PythonImage },
+  ];
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -56,8 +72,17 @@ export default function SkillsPage() {
           </TabSelector>
         </div>
       </div>
-      <div>
-        <TabPanel hidden={selectedTab !== "Language"}>Language</TabPanel>
+      <div className="contentPanel">
+        <TabPanel hidden={selectedTab !== "Language"}>
+          <div className="scrolling-wrapper-flexbox">
+            {languages.map((language, index) => (
+              <div key={language} className="card">
+                <img src={language.image} alt={language.name} />
+                <p className="cardName">{language.name}</p>
+              </div>
+            ))}
+          </div>
+        </TabPanel>
         <TabPanel hidden={selectedTab !== "Frameworks"}>Frameworks</TabPanel>
         <TabPanel hidden={selectedTab !== "IDE"}>IDE</TabPanel>
         <TabPanel hidden={selectedTab !== "Database"}>Billing</TabPanel>
